9b85cdd07a4d4230c53e1bb6817a36729e145d9a
ffacon <francois.facon@gmail.com>
2015-03-03 21:28:18 +0100

    Add Detail Tests

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -31,8 +31,8 @@ module.exports = function (config) {
             '../bower_components/**/*file.js',
             '../bower_components/**/index.js',
             '../bower_components/**/*.min.js',
-            '../bower_components/angular-translate-loader-*/*.*',
-            '../bower_components/angular-translate-storage-*/*.*'
+            '../bower_components/angular-translate-loader-*/**/*.*',
+            '../bower_components/angular-translate-storage-*/**/*.*'
             
         ],
 
@@ -62,7 +62,7 @@ module.exports = function (config) {
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
+        logLevel: config.LOG_DEBUG,
 
 
         // enable / disable watching file and executing tests whenever any file changes
---
+++ test/e2e/DetailBookSpec.js
@@ -0,0 +1,27 @@
+describe("Detail Book Page", function () {
+
+    beforeEach(function(){
+        browser.get('http://localhost:8080/#/book/1');
+    });
+
+    it("Check Book Name", function () {
+        expect(element(by.binding('product.name')).getText()).toEqual('ANGULARJS');
+    });
+
+    it('The price change when updating quantity', function () {
+        expect(element(by.binding('product.price * quantity')).getText()).toEqual('$15.34');
+        var qty = element(by.model('quantity'));
+        qty.clear();
+        /*browser.debugger();*/
+        qty.sendKeys(2);
+        expect(element(by.binding('product.price * quantity')).getText()).toEqual('$30.68');
+    });
+
+
+
+    it('The image url is correct', function () {
+        browser.debugger();
+        expect(element(by.css('.photo')).getAttribute('src')).toEqual('http://localhost:3000/img/catalog/1.jpg');
+    });
+
+});
